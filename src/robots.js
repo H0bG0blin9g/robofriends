3eda4720b7ee9a7b77c8dfb8c604c5f68abdf516
H0bG0blin9g <h0bg0blin9g@gmail.com>
2020-09-26 21:15:46 +0100

    delete unncessary react css and js files.  Also deleted the Hello js and css files.  Have created the robots and cards file.

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'nathan@yesnia.net'
+    },
+    {
+        id: 4,
+        name: 'Patrick Lebsack',
+        username: 'Karianne',
+        email: 'julianne.oconner@kory.org'
+    },
+    {
+        id: 5,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'sherwood@rosamond.me'
+    }
+]
